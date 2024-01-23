7fe98ac76d7f9f4f59730aa1c61866b0631c02d2
vanh <hoangthivananh22122002@gmail.com>
2024-01-23 13:44:28 +0700

    ..

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ import mongoose from 'mongoose';
 import routers from './Student/studentRouter.js';
 import bodyParser from 'body-parser';
 dotenv.config();
-
 const port = process.env.PORT || 30000;
 const app = express();
 app.use(bodyParser.json())
-mongoose.connect('mongodb://127.0.0.1:27017/Student').then(() => console.log('Kết nối thành công'));
+mongoose.connect('mongodb+srv://vanh:56UJVZAuyTUgAra9@cluster0.ds6mjsp.mongodb.net/').then(() => console.log('Kết nối thành công'));
 app.use('/student',routers);
 app.listen(port, () => {
     console.log('PORT', port);
